22f47628ca3389590e794b2ac3df00227a2c7219
Aituni <52550106+Aituni@users.noreply.github.com>
2019-11-27 16:34:40 +0100

    Cosas
    Comprar php

---
+++ filtrar.js
@@ -0,0 +1,18 @@
+function filtrar(f){
+	var tipo;
+
+	if(document.getElementById('viento').checked) {
+		
+	}else if(document.getElementById('percusion').checked) {
+
+	}else if(document.getElementById('cuerda').checked) {
+
+	}else if(document.getElementById('electrico').checked) {
+
+	}else if(document.getElementById('compelementos').checked) {
+
+	}else if(document.getElementById('todos').checked) {
+
+	}
+
+}
