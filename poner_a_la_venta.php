a43c4f99fcafd0e234ea40060722a5fe6344a9e8
Aitor <aituniehu@gmail.com>
2019-12-06 19:02:21 +0100

    Validación en el servidor

--- poner_a_la_venta.php
+++ poner_a_la_venta.php
@@ -5,73 +5,66 @@
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="estilo.css">
 	</head>
-<!--
-    <pre>
-        <?php
-        function br() { echo '<br />'; }
 
-        echo 'post: ';
-        print_r($_POST);
-        br();
-        echo 'get: ';
-        print_r($_GET);
-        br();
-        echo 'cookie: ';
-        print_r($_COOKIE);
-        br();
-        echo 'files: ';
-        print_r($_FILES);
-        br();
-        echo 'request: ';
-        print_r($_REQUEST);
-        ?>  
-    </pre>
--->
 	<body>
 <?php
+    $continue=False;
+    if (isset($_POST['username'])  || isset($_POST['correo']) || isset($_POST['telefono']) || isset($_POST['precio']) || isset($_POST['categoria']) || isset($_POST['descripcion']) || isset($_POST['precio']) || isset($_POST['productname']) ){
 
-    $username=$_POST['username'];
-    $correo=$_POST['correo'];
-    $telefono=$_POST['telefono'];
-    $precio=$_POST['precio'];
-    $categoria=$_POST['categoria'];
-    $descripcion=$_POST['descripcion'];
+        $username=$_POST['username'];
+        $correo=$_POST['correo'];
+        $telefono=$_POST['telefono'];
+        $precio=$_POST['precio'];
+        $categoria=$_POST['categoria'];
+        $descripcion=$_POST['descripcion'];
+        $precio=$_POST['precio'];
+        $productname=$_POST['productname'];
+        
+        if (is_numeric($telefono) && $telefono>0 && is_numeric($precio) && strlen((string)$telefono) == 9 ){
+            if(isset($_FILES["archivo"]) && $_FILES["archivo"]["name"][0]){
+                $elements = explode("@", $correo);
+                if ( count($elements)==2 ){
+                    if( strlen($elements[0])>2 && strlen($elements[1])>2 ){
+                        $cServ = explode(".",$elements[1]);
+                        if (count($cServ)>=2 ){
+                            if( strlen($cServ[count($cServ)-1])>2 ){
+                                $hasnum=False;
+                                $continue=True;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    $precio=$_POST['precio'];
-    $productname=$_POST['productname'];
+    if ($continue){
 
-    $fecha=date('r');
-    $numImg=0;
+        $fecha=date('r');
+        $numImg=0;
 
-    $productos=simplexml_load_file('productos.xml');
-    //TODO: guardar imagen
-    $ult_id = $productos['ult_id'];
-    $ult_id = $ult_id +1;
-    $productos['ult_id']=$ult_id;
+        $productos=simplexml_load_file('productos.xml');
 
-    $nuevo = $productos->addChild('producto');
-    $nuevo['id'] = $ult_id;
-    $nuevo['categoria'] = $categoria;
+        $ult_id = $productos['ult_id'];
+        $ult_id = $ult_id +1;
+        $productos['ult_id']=$ult_id;
 
-    $nuevo->addChild('username',$username);
-    $nuevo->addChild('productname', $productname);
+        $nuevo = $productos->addChild('producto');
+        $nuevo['id'] = $ult_id;
+        $nuevo['categoria'] = $categoria;
 
-    $nuevo->addChild('descripcion',$descripcion);
-    $nuevo->addChild('precio',$precio);
-    $nuevo->addChild('email',$correo);
-    $nuevo->addChild('telefono',$telefono);
-    $nuevo->addChild('fecha',$fecha);
+        $nuevo->addChild('username',$username);
+        $nuevo->addChild('productname', $productname);
+        $nuevo->addChild('descripcion',$descripcion);
+        $nuevo->addChild('precio',$precio);
+        $nuevo->addChild('email',$correo);
+        $nuevo->addChild('telefono',$telefono);
+        $nuevo->addChild('fecha',$fecha);
 
+        # definimos la carpeta destino
+        $carpetaDestino="images/";     
 
-
-
-    # definimos la carpeta destino
-    $carpetaDestino="images/";
-
-    # si hay algun archivo que subir
-    if(isset($_FILES["archivo"]) && $_FILES["archivo"]["name"][0]){
-
-	   $numImg=count($_FILES["archivo"]["name"]);
+	    $numImg=count($_FILES["archivo"]["name"]);
 
         # recorremos todos los arhivos que se han subido
         for($i=0;$i<$numImg;$i++)
@@ -92,27 +85,36 @@
  
                     # movemos el archivo
                     if(!@move_uploaded_file($origen, $destino))
-                    {                        
+                    {                   
+                        $numImg=-1;     
                         echo "<br> Error en el servidor con el archivo:".$oldname;
                     }
                 }else{
-                    echo "<br> Debido a un problema en el servidor, no se han podido guardar las imagenes: ";
+                    $numImg=-1;
+                    echo "<br> Debido a un problema en el servidor, no se han podido guardar las imágenes ";
                 }
             }else{
+                $numImg=-1;
                 echo "<br>".$oldname." - NO es imagen jpg, png o gif. Por lo que ha sido rechazado por el servidor";
             }
         }
-    }else{
-        echo "<br>No se ha subido ninguna imagen";
-    }
-    $nuevo->addChild('numImg',$numImg);
-    $productos->asXML('productos.xml');
+        $nuevo->addChild('numImg',$numImg);
+        $productos->asXML('productos.xml');
 
-    echo "<br> <h2>Información guardada.</h2>";
+        echo "<br> Información guardada.";
+
+    }else{
+        echo "<br> Los datos no estan completos, o no respetan las normas impuestas.";
+    }
 ?>
 		<div class="title">
-			<h1>El producto ya esta disponible en la tienda.</h1>
-			<a href="index.html">Haz click aqui para volver al inicio.</a>
+            <?php
+            if ($continue){
+                echo "<h1>El producto ya está disponible en la tienda.</h1>";
+            }
+            echo '<a href="vender.html">Haz click aqui para volver a vender otro producto</a><br>';
+            echo '<a href="index.html">Haz click aqui para volver al inicio.</a>';
+            ?>
 		</div>
 	</body>
 </html>
